af6b2029d838b556def686271d02cbe4281689bd
kai <supon3060@gmail.com>
2023-12-10 15:28:00 +0800

    Update instantiate scripts

--- instantiate.jl
+++ instantiate.jl
@@ -1,29 +1,29 @@
 using Pkg
 using CondaPkg
 
+# Helper functions
+function win_path_to_unix_path(path::AbstractString)
+    drive, path = splitdrive(normpath(path))
+    return "/" * lowercase(drive[1]) * replace(path, "\\" => "/")
+end
+
 # Parse ARGS
 args_len = length(ARGS)
 if args_len == 0
     force_resolve = false
-    skip_install = false
 elseif args_len == 1
     force_resolve = parse(Bool, ARGS[1])
-    skip_install = false
-elseif args_len == 2
-    force_resolve = parse(Bool, ARGS[1])
-    skip_install = parse(Bool, ARGS[2])
 end
 
 # Instantiate
-if skip_install
-    Pkg.instantiate()
-    CondaPkg.resolve(; force=force_resolve)
-end
+Pkg.instantiate()
+CondaPkg.resolve(; force=force_resolve)
+
 
 # Write .env
 CondaPkg.backend() in (:Null, :Current) && return
 open(".env", "w") do io
-    if Sys.iswindows()  # TODO: test
+    if Sys.iswindows()
         path_sep = ':'
         old_path = join(
             map(
@@ -47,40 +47,23 @@ open(".env", "w") do io
         if old_path != ""
             new_path = "$(new_path)$(path_sep)$(old_path)"
         end
-        println(io, "PATH=$(new_path)")
+        println(io, "PATH=\"$(new_path)\"")
         println(io, "CONDA_PREFIX=$(env_dir)")
         println(io, "CONDA_DEFAULT_ENV=$(env_dir)")
         println(io, "CONDA_SHLVL=1")
         println(io, "CONDA_PROMPT_MODIFIER=($(env_dir))")
-        println(io, "alias conda='$(win_path_to_unix_path(join(CondaPkg.conda_cmd().exec, " "))) -p $(env_dir)'")
+        println(io, "alias conda=\"$(win_path_to_unix_path(join(CondaPkg.conda_cmd().exec, " "))) -p $(env_dir)\"")
     else
         CondaPkg.withenv() do
             if CondaPkg.backend() == :MicroMamba
                 println(io, "MAMBA_ROOT_PREFIX=$(ENV["MAMBA_ROOT_PREFIX"])")
             end
-            println(io, "PATH=$(ENV["PATH"])")
+            println(io, "PATH=\"$(ENV["PATH"])\"")
             println(io, "CONDA_PREFIX=$(ENV["CONDA_PREFIX"])")
             println(io, "CONDA_DEFAULT_ENV=$(ENV["CONDA_DEFAULT_ENV"])")
             println(io, "CONDA_SHLVL=$(ENV["CONDA_SHLVL"])")
             println(io, "CONDA_PROMPT_MODIFIER=$(ENV["CONDA_PROMPT_MODIFIER"])")
-            println(io, "alias conda='$(join(CondaPkg.conda_cmd().exec, " ")) -p $(CondaPkg.STATE.conda_env)'")
+            println(io, "alias conda=\"$(join(CondaPkg.conda_cmd().exec, " ")) -p $(CondaPkg.STATE.conda_env)\"")
         end
     end
 end
-
-function win_path_to_unix_path(path::String)
-    drive, path = splitdrive(normpath(path))
-    return "/" * lowercase(drive[1]) * replace(path, "\\" => "/")
-end
-
-# TODO: remove
-# CondaPkg.withenv() do
-#     open(".env", "w") do io
-#         println(io, "PATH=$(ENV["PATH"])")
-#         println(io, "CONDA_PREFIX=$(ENV["CONDA_PREFIX"])")
-#         println(io, "CONDA_DEFAULT_ENV=$(ENV["CONDA_DEFAULT_ENV"])")
-#         println(io, "CONDA_SHLVL=$(ENV["CONDA_SHLVL"])")
-#         println(io, "CONDA_PROMPT_MODIFIER=$(ENV["CONDA_PROMPT_MODIFIER"])")
-#         println(io, "alias conda='$(join(CondaPkg.conda_cmd().exec, " ")) -p $(CondaPkg.STATE.conda_env)'")
-#     end
-# end
--- instantiate.sh
+++ instantiate.sh
@@ -3,7 +3,6 @@
 # Parse command line arguments
 # Ref: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 FORCE_RESOLVE="false"
-SKIP_INSTALL="false"
 SOURCE_DOTENV="true"
 # As long as there is at least one more argument, keep looping
 while [[ $# -gt 0 ]]; do
@@ -12,10 +11,6 @@ while [[ $# -gt 0 ]]; do
     -f|--force-resolve)
     shift # past the key and to the value
     FORCE_RESOLVE="$1"
-    ;;
-    -s|--skip-install)
-    shift # past the key and to the value
-    SKIP_INSTALL="$1"
     ;;
     -d|--source-dotenv)
     shift # past the key and to the value
@@ -31,7 +26,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 # On Windows, this scripts only support msys(Git Bash) or cygwin
-julia --project=. --startup-file=no instantiate.jl "$FORCE_RESOLVE" "$SKIP_INSTALL"
+julia --project=. --startup-file=no instantiate.jl "$FORCE_RESOLVE"
 
 
 if [ "$SOURCE_DOTENV" ]; then
